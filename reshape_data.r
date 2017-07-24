d2db3f39c61605ebb24418077c3a179f7a01bc34
mooromets <sergey.sambor@gmail.com>
2017-07-24 16:09:33 +0200

    team points added in reshaping

--- reshape_data.r
+++ reshape_data.r
@@ -5,8 +5,12 @@ library(lubridate)
 
 source("./get_epl_data.r")
 
-data <- readSeasons(seasonFiles(2010:2017, 1:2))
+#data <- d <- readSeasonsData(2012:2017,c(1,2), c("Div", "Date", "HomeTeam", "AwayTeam", "FTR"))
 
 # convert 'Date' variable to a Date format and add 'year' variable 
 data <- mutate(data, Date = as.Date(as.character(Date), "%d/%m/%Y"),
             year = ifelse(month(Date) > 7, year(Date)+1, year(Date)))
+
+# add points
+data <- mutate(data, HoPts = ifelse(FTR == 'H', 3, ifelse(FTR == 'D', 1, 0)),
+                    AwPts = ifelse(FTR == 'A', 3, ifelse(FTR == 'D', 1, 0)))
