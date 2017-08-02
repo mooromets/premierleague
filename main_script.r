55ab2c5a122dffa3c672a747983d139430426f39
mooromets <sergey.sambor@gmail.com>
2017-08-02 18:26:50 +0200

    tune up plotting

--- main_script.r
+++ main_script.r
@@ -3,13 +3,15 @@ source("./newcomers_performance.r")
 source("./reshape_data.r")
 
 
-downloadSeasons(2005:2017,1:2)
+downloadSeasons(2001:2017,1:2)
 
-data <- readSeasonsData(2005:2017, c(1,2), c("Div", "Date", "HomeTeam", "AwayTeam", "FTR"))
+data <- readSeasonsData(2001:2017, c(1,2), c("Div", "Date", "HomeTeam", "AwayTeam", "FTR"))
 
 data <- basicDataClean(data)
 
-stats <- do.call("rbind", lapply (2006:2017, getNewcomersStats, data))
+stats <- do.call("rbind", lapply (2002:2017, getNewcomersStats, data))
+stats$Div2.place <- as.factor(stats$Div2.place)
+stats$Div1.year <- as.factor(stats$Div1.year)
 
 plot(stats$Pts.Div2, stats$Pts.Div1)
 
@@ -21,4 +23,3 @@ library(ggplot2)
 
 g <- ggplot(data = stats, aes(x = Div1.year, y = Pts.Div1, group = Div2.place))
 g + geom_line(aes(color = Div2.place))
-
