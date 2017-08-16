1bd2f9ea4975e396e0d41bb0c85ac0c1d7b23a69
mooromets <sergey.sambor@gmail.com>
2017-08-16 23:27:29 +0200

    added: calculation of PPG-form

--- form.r
+++ form.r
@@ -24,6 +24,43 @@ ppg_pl <- function(data, fromDate, toDate, teams) {
   #TODO: not simply replace small values, but append averages to existing Pts and games
   ppg %>%
     mutate(PPG = Pts / games) %>%
-    select(team, field, PPG, games) %>%
+    select(team, field, PPG, games, Pts) %>% #TODO remove Pts ?
     arrange(field, -PPG)
 }
+
+#calculate from on PPG for a team in selected season
+calc_form_ppg <- function(tm, fld, season_state) {
+  oppField <- ifelse(fld == 'A', 'H', 'A')
+  last_n_games <- season_state$data %>%
+    filter(field == fld & team == tm & Div == season_state$Div & Date > season_state$fromDate) %>%
+    top_n(season_state$num_games, Date)
+  form_result <- 
+    left_join(data.frame(team = last_n_games$opp, field = rep(oppField, season_state$num_games)), 
+              season_state$season_form,
+              by = c("team", "field")) %>%
+    full_join(., 
+              last_n_games, 
+              by = c("team" = "opp"), 
+              suffix = c(".opp.form", ".team"))
+  sum (form_result$Pts.team * form_result$PPG)
+}
+  
+
+#current form, calculated from latest games and ppg of those oppenents
+form_ppg <- function(data, num_games, fromDate, toDate, teams) {
+  season_state <- list()
+  season_state$num_games <- num_games #either home or away
+  season_state$all_teams <- unique(as.data.frame(data %>% filter (Div == "E0" & Date > fromDate & Date < toDate))[,"team"])
+  season_state$season_form <- ppg_pl(data, fromDate, toDate, all_teams)
+  season_state$data <- data
+  season_state$fromDate <- fromDate
+  season_state$toDate <- toDate
+  season_state$Div <- "E0"
+
+  rbind(
+    do.call("rbind", lapply(teams, calc_form_ppg, 'A', season_state)),
+    do.call("rbind", lapply(teams, calc_form_ppg, 'H', season_state))
+  )
+}
+
+form_ppg(data, num_games, fromDate, toDate, c("Liverpool", "Watford"))
