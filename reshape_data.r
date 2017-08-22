b85cdc7388306da3c59d14c804ac1e7e759bc014
mooromets <sergey.sambor@gmail.com>
2017-08-22 23:28:18 +0200

    removed code duplication in basicDataClean()

--- reshape_data.r
+++ reshape_data.r
@@ -8,19 +8,21 @@ basicDataClean <- function(data) {
   data <- mutate(data, Date = dmy(Date), year = ifelse(month(Date) > 7, 
                                                        year(Date)+1, year(Date)))
   
-  # make two columns HomeTeam and AwayTeam into single one
-  rbind (
-    # Home
+  selectBySide <- function (side) {
+    if (side == 'H') {
+      thisTeam <- "HomeTeam"
+      oppTeam <- "AwayTeam"
+    } else if (side == 'A') {
+      thisTeam <- "AwayTeam"
+      oppTeam <- "HomeTeam"      
+    }
     select(data, c("Div", "Date", "year", "HomeTeam", "FTR", "AwayTeam")) %>%
-      mutate(field = "H", 
-             FTR = ifelse(FTR == 'H', 'W', ifelse(FTR == 'D', 'D', 'L')),
+      mutate(field = side, 
+             FTR = ifelse(FTR == side, 'W', ifelse(FTR == 'D', 'D', 'L')),
              Pts = ifelse(FTR == 'W', 3, ifelse(FTR == 'D', 1, 0)) ) %>%
-      rename (team = HomeTeam, opp = AwayTeam),
-    # Away
-    select(data, c("Div", "Date", "year", "AwayTeam", "FTR", "HomeTeam")) %>%
-      mutate(field = "A", 
-             FTR = ifelse(FTR == 'A', 'W', ifelse(FTR == 'D', 'D', 'L')),
-             Pts = ifelse(FTR == 'W', 3, ifelse(FTR == 'D', 1, 0)) ) %>%
-      rename (team = AwayTeam, opp = HomeTeam)
-  )
+      rename_(team = thisTeam, opp = oppTeam)    
+  }
+  
+  # make two columns HomeTeam and AwayTeam into single one
+  rbind (  selectBySide('H'), selectBySide('A'))
 }
