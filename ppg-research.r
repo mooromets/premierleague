39faf32e91adbf9488f38c3cee708268aa70189c
mooromets <sergey.sambor@gmail.com>
2017-08-30 19:23:44 +0200

    added the calculation of rel PPG for all teams in train data

--- ppg-research.r
+++ ppg-research.r
@@ -9,12 +9,12 @@ form_games_num <- 5
 set.seed(278)
 seasons <- 2005:2017
 train_set <- sample(seasons, 7)
-test_set <- sample(setdiff(seasons, train), 3)
-validation_set <- setdiff(seasons, union(train, test))
+test_set <- sample(setdiff(seasons, train_set), 3)
+validation_set <- setdiff(seasons, union(train_set, test_set))
 
 # getting data
-downloadSeasons(2005:2017,1:2)
-data <- readSeasonsData(2005:2017, c(1,2), c("Div", "Date", "HomeTeam", "AwayTeam", "FTR"))
+downloadSeasons(2003:2017,1:2)
+data <- readSeasonsData(2003:2017, c(1,2), c("Div", "Date", "HomeTeam", "AwayTeam", "FTR"))
 data <- basicDataClean(data)
 
 train_data  <- data %>%
@@ -23,7 +23,43 @@ train_data  <- data %>%
            year == train_set[3] | year == train_set[4] | year == train_set[5] | 
            year == train_set[6] | year == train_set[7])
 
-# !!! using (full) data for calculation features
-points_sprint_relative_speed(data, form_games_num, divEPL, ymd("2015-10-20"), 
-                             c("Liverpool", "Chelsea", "Arsenal", "Everton"), 
-                             rulesEPL)
+out <- data.frame ()
+
+all_matchdays <- unique(train_data[train_data$Div == divEPL, "Date"]) [1:300]
+for (matchday in all_matchdays) {
+  #progress 
+  print(round(match(matchday, all_matchdays) / length(all_matchdays), 4))
+  
+  matchday <- as.Date(matchday, origin = "1970-01-01")
+
+  games <- data %>% filter(Div == divEPL, Date == matchday, field == 'H')
+  games$team <- as.character(games$team)
+  games$opp <- as.character(games$opp)
+
+  # !!! using (full) data for calculation features, not train data
+  relPPG <-  
+    points_sprint_relative_speed(data, 
+                                     form_games_num, 
+                                     divEPL,
+                                     as.Date(matchday, origin = "1970-01-01"), 
+                                     c(games$team, games$opp), 
+                                     rulesEPL)
+
+  games <- left_join(games, relPPG, by = c("team", "field"))
+  
+  games <- left_join(games, 
+            relPPG[relPPG$field == 'A',], #opposition always away 
+            by = c("opp" = "team"),
+            suffix = c(".team", ".opp"))
+  
+  out <- rbind(out, games[,c("year", "FTR", "ppg_rel.team", "ppg_rel.opp")])
+}
+
+clean <- out[complete.cases(out),]
+clean <- clean[clean$ppg_rel.team != 0, ] #dunno what to do with zeros at the moment
+clean <- clean[clean$ppg_rel.opp != 0, ]
+clean$k <- clean$ppg_rel.team/clean$ppg_rel.opp
+
+qplot(clean$k, colour = clean$FTR)
+qplot(clean$k)+facet_grid(. ~ clean$FTR)
+qplot(clean$ppg_rel.team, clean$ppg_rel.opp, colour = clean$FTR)
